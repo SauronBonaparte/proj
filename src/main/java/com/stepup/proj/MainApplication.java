aa82ae700994d2eb0114a3aed20ddf25cd2b4b31
Vladimir Danilov <dvv1990@gmail.com>
2024-07-22 20:09:42 +0300

    нихуя не сделали

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -7,6 +7,7 @@ public class MainApplication {
         System.out.println("22222!");
         System.out.println("33333!");
         System.out.println("55555!");
+        System.out.println("-----");
     }
-    public static void doSomethig
+
 }
