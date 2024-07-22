1cd4545b7e65179d66d0a5468f2722ce6a1d538d
Vladimir Danilov <dvv1990@gmail.com>
2024-07-22 20:14:08 +0300

    нихуя не сделали

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,10 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args){
         System.out.println("0!");
-        System.out.println("0!");
-        System.out.println("0!");
-        System.out.println("0!");
-        System.out.println("0!");
+        System.out.println("1!");
+        System.out.println("1!");
+        System.out.println("1!");
+        System.out.println("1!");
+        System.out.println("--");
     }
 
 }
